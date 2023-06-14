4f34c79e917f0119dc296c345df88ee68aa4d6c9
nsaraiva <nuno.saraiva@wacestudio.com>
2023-06-14 13:30:09 +0100

    dev

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "03b49674b4edb910c0561db4b0d3624ce2a910a379ebf7b5d7b1894ee49b9135fa": 100,
+  "035486230f5d65bbf37d382796a0966702f5cddf4c22f80d07fd5c47d2f44a71f1": 50,
+  "033ebc1430e75f22df00232402f5cc6912e5c340076bd0e660a5962310896b57ff": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
